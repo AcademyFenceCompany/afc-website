dc4b3130116319f720027b3bb08abebe19cb947a
AcademyFenceCompany <info@academyfence.com>
2024-12-09 16:24:48 -0500

    Updated product controller - welded wire

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\FamilyCategory;
 use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
 
@@ -19,10 +20,16 @@ class ProductController extends Controller
         }
 
         // Fetch products related to the Welded Wire category
-        $products = Product::where('family_category_id', $weldedWireCategory->product_id)
-        ->with(['productDetail', 'productMedia'])
-        ->get();
-    
-        return view('categories.weldedwire', compact('weldedWireCategory', 'products'));
+        // $products = Product::where('family_category_id', $weldedWireCategory->product_id)
+        //     ->with(['productDetail', 'productMedia'])
+        //     ->get();
+
+        $products_ww = DB::table('products')
+            ->join('product_details', 'products.product_id', '=', 'product_details.product_id')
+            ->join('product_media', 'products.product_id', '=', 'product_media.product_id')
+            ->select($columns = ['*'])
+            ->get();
+
+        return view('categories.weldedwire', compact('weldedWireCategory', 'products_ww'));
     }
 }
