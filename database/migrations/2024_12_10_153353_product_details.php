4a052925fa811af41ab4152efc4a6a082934dc59
AcademyFenceCompany <info@academyfence.com>
2024-12-10 10:40:16 -0500

    Corrected comments of size columns on product_details table

---
+++ database/migrations/2024_12_10_153353_product_details.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('product_details', function (Blueprint $table) {
+            $table->string('size1')->comment('Length x Width Regardless of Category')->change();
+            $table->string('size2')->comment('Welded Wire: Mesh Size')->change();
+            $table->string('size3')->comment('Welded Wire: Gauge Thickness')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
