da6d65de1fb54db12da2e610d77371bd473679b9
Sunny <sunny.academyfence@gmail.com>
2025-01-10 11:13:35 -0500

    user-management-categorymanagement-add product features merge here

--- resources/views/layouts/ams.blade.php
+++ resources/views/layouts/ams.blade.php
@@ -38,7 +38,6 @@
             <a href="{{ route('ams.products.add') }}" class="menu-item">Add Product</a>
             <a href="{{ route('products.index') }}" class="menu-item">View Products</a>
             <a href="{{ route('categories.display') }}" class="menu-item">Category Management</a>
-            <a href="#" class="menu-item">Out of Stock</a>
         </div>
 
         <!-- Customers -->
