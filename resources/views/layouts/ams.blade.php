25a46a61d37b3ee740693b3c6292bd588f360c6c
Sunny <sunny.academyfence@gmail.com>
2025-01-10 10:08:52 -0500

    edit products working

--- resources/views/layouts/ams.blade.php
+++ resources/views/layouts/ams.blade.php
@@ -109,6 +109,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('js/user-management.js') }}"></script>
+    <script src="{{ asset('js/ams.js') }}"></script>
 
 </body>
 
