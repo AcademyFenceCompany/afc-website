2782c6c4ead1248f2143ab17c9c2f510b16f526f
AcademyFenceCompany <info@academyfence.com>
2025-02-10 13:13:54 -0500

    added structure of view shippers

--- resources/views/ams/shipping/index_shippers.blade.php
+++ resources/views/ams/shipping/index_shippers.blade.php
@@ -4,6 +4,51 @@
 
 @section('content')
 
-<h1>Shippers</h1>
+<div class="container">
+    <!-- Page Title -->
+    <div class="row add_product__title text-center">
+        <h2>VIEW SHIPPERS</h2>
+    </div>
+
+    <!-- Search Form -->
+    <div class="row">
+        <div class="col-md-6">         
+                <label for="shipper-search" class="form-label">Search:</label>
+                <div class="input-group">
+                    <input type="text" id="shipper-search" class="form-control" placeholder="Enter Shipper search key.....">
+                    <button id="searchButton" class="btn btn-primary">Search</button>
+                </div>
+        </div>
+    </div>
+
+
+    <div class="container mt-1">
+        <!-- Table Header -->
+        <div class="row table-header text-white p-3">
+            <div class="col-1">#</div>
+            <div class="col-3">Shipper's Name</div>
+            <div class="col-2">Shipping Type</div>
+            <div class="col-2">Dispatcher</div>
+            <div class="col-3">Dispatcher Phone Number</div>
+            <div class="col-1 text-center">Action</div>
+        </div>
+
+        <!-- Table Row -->
+        <div class="row table-row align-items-center p-1">
+            <div class="col-1">1</div>
+            <div class="col-3">Shipper's Name</div>
+            <div class="col-2">Shipping Type</div>
+            <div class="col-2">Dispatcher</div>
+            <div class="col-3">Dispatcher Phone Number</div>
+            <div class="col-1 text-center">
+                <div class="text-center mt-1 button-group">
+                    <button class="btn btn-primary btn-sm">Edit</button>
+                    <button class="btn btn-secondary btn-sm">View</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 @endsection
