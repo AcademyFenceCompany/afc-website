29dd2014d14d0b71411e89ce68d05cae083f5e64
Academy Fence Company <info@academyfence.com>
2025-04-24 10:28:06 -0400

    fixing header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -124,30 +124,28 @@
                 </button>
 
                 <!-- Desktop Navigation -->
-                <nav class="nav mb-3 d-none d-md-flex flex-wrap">
+                <nav class="nav mb-3 d-none d-md-flex flex-nowrap overflow-auto">
                     <!-- Wood Fence Dropdown -->
                     <div class="dropdown">
-                        <li><a href="{{ route('woodfence') }}" class="nav-link btn nav-btn dropdown-toggle" data-bs-toggle="">WOOD FENCE</a></li>
+                        <a href="{{ route('woodfence') }}" class="nav-link btn nav-btn dropdown-toggle" data-bs-toggle="dropdown">WOOD FENCE</a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="/wood-fence/specs/6/2%201/2%20in.?styleTitle=Spaced+Picket">Spaced Picket Fence</a></li>
                             <li><a class="dropdown-item" href="{{ route('solid-board') }}">Solid Board Fence</a></li>
-                            <li><a class="dropdown-item" href="{{ route('board-on-board') }}">Shadow Box Board On
-                                    Board</a></li>
+                            <li><a class="dropdown-item" href="{{ route('board-on-board') }}">Shadow Box Board On Board</a></li>
                             <li><a class="dropdown-item" href="{{ route('stockade.index') }}">Stockade Fence</a></li>
                             <li><a class="dropdown-item" href="{{ route('board-on-board') }}">Board Fence</a></li>
-                            <li><a class="dropdown-item" href="{{ route('tongue-groove') }}">Tongue Groove Fence</a>
-                            </li>
+                            <li><a class="dropdown-item" href="{{ route('tongue-groove') }}">Tongue Groove Fence</a></li>
                             <li><a class="dropdown-item" href="{{ route('postrail.index') }}">Posts & Rails</a></li>
                             <li><a class="dropdown-item" href="{{ route('woodpostcaps.index') }}">Woodpost Caps</a></li>
                             <li><a class="dropdown-item" href="">Loose Wood Fence</a></li>
-
                         </ul>
                     </div>
 
                     <a href="#" class="nav-link btn nav-btn">VINYL FENCE</a>
+                    
                     <!-- Chain Link Fence Dropdown -->
                     <div class="dropdown">
-                        <a href="{{ route('chainlink.main') }}" class="nav-link btn nav-btn dropdown-toggle" data-bs-toggle="">CHAIN LINK</a>
+                        <a href="{{ route('chainlink.main') }}" class="nav-link btn nav-btn dropdown-toggle" data-bs-toggle="dropdown">CHAIN LINK</a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="{{ route('chainlink.height', ['height' => '4ft']) }}">4ft Chain Link Fence</a></li>
                             <li><a class="dropdown-item" href="{{ route('chainlink.height', ['height' => '5ft']) }}">5ft Chain Link Fence</a></li>
@@ -157,75 +155,34 @@
                     
                     <!-- Aluminum Fence Dropdown -->
                     <div class="dropdown">
-                        <a href="{{ route('aluminumfence.main') }}" class="nav-link btn nav-btn dropdown-toggle" data-bs-toggle="">ALUMINUM FENCE</a>
+                        <a href="{{ route('aluminumfence.main') }}" class="nav-link btn nav-btn dropdown-toggle" data-bs-toggle="dropdown">ALUMINUM FENCE</a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="{{ route('aluminumfence.index') }}">OnGuard Aluminum Fence</a></li>
                             <li><a class="dropdown-item" href="{{ route('aluminumfence.pickup') }}">Available for Pickup</a></li>
                         </ul>
                     </div>
 
-
                     <div class="dropdown">
-                        <a href='{{ route('weldedwire') }}' class="nav-link btn nav-btn dropdown-toggle">WELDED WIRE</a>
+                        <a href='{{ route('weldedwire') }}' class="nav-link btn nav-btn dropdown-toggle" data-bs-toggle="dropdown">WELDED WIRE</a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=4%2Bin.%2Bx%2B4%2Bin.&amp;coating=Vinyl%20PVC">4"
-                                    x 4"</a></li>
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=3%2Bin.%2Bx%2B3%2Bin.&amp;coating=Vinyl%20PVC">3"
-                                    x 3"</a></li>
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=2%2Bin.%2Bx%2B4%2Bin.&amp;coating=Vinyl%20PVC">2"
-                                    x 4"</a></li>
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=2%2Bin.%2Bx%2B2%2Bin.&amp;coating=Vinyl%20PVC">2"
-                                    x 2"</a></li>
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=2%2Bin.%2Bx%2B2%2Bin.&amp;coating=Vinyl%20PVC">1
-                                    1/2" x 1 1/2"</a></li>
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=1%2B1%252F2%2Bin.%2Bx%2B4%2Bin.&amp;coating=Vinyl%20PVC">1
-                                    1/2" x 4"</a></li>
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=3%2Bin.%2Bx%2B2%2Bin.&amp;coating=Vinyl%20PVC">3"
-                                    x 2"</a></li>
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=1%2Bin.%2Bx%2B3%2Bin.&amp;coating=Vinyl%20PVC">1"
-                                    x 3"</a></li>
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=1%2Bin.%2Bx%2B2%2Bin.&amp;coating=Vinyl%20PVC">1"
-                                    x 2"</a></li>
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=1%2Bin.%2Bx%2B1%2Bin.&amp;coating=Vinyl%20PVC">1"
-                                    x 1"</a></li>
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=1%252F2%2Bin.%2Bx%2B1%2Bin.&amp;coating=Vinyl%20PVC">1/2"
-                                    x 1"</a></li>
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=1%252F2%2Bin.%2Bx%2B1%252F2%2Bin.&amp;coating=Vinyl%20PVC">1/2"
-                                    Hardware Cloth</a></li>
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=1%252F4%2Bin.%2Bx%2B1%252F4%2Bin.&amp;coating=Vinyl%20PVC">1/4"
-                                    Hardware Cloth</a></li>
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=1%252F8%2Bin.%2Bx%2B1%252F8%2Bin.&amp;coating=Vinyl%20PVC">1/8"
-                                    Hardware Cloth</a></li>
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=1%2Bin.%2Bx%2B1%2Bin.&coating=Vinyl%20PVC">1"
-                                    Hex Netting/Chicken Wire</a></li>
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=2%2Bin.%2Bx%2B2%2Bin.&coating=Vinyl%20PVC">2"
-                                    Hex Netting/Chicken Wire</a></li>
-                            <!-- <li><a class="dropdown-item" href="#">Deer Fence</a></li> -->
-                            <!-- <li><a class="dropdown-item" href="#">Welded Wire Gates</a></li> -->
-                            <!-- <li><a class="dropdown-item" href="#">Silt/Erosion Fence</a></li> -->
-                            <!-- <li><a class="dropdown-item" href="#">Snow Fence</a></li> -->
-                            <li><a class="dropdown-item"
-                                    href="http://192.168.0.135/wwf-product?meshSize=4%2Bin.%2Bx%2B4%2Bin.&amp;coating=Galvanized">Knock-In
-                                    Post</a></li>
-                            <!-- <li><a class="dropdown-item" href="#">Specialty Wire</a></li> -->
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=4%2Bin.%2Bx%2B4%2Bin.&amp;coating=Vinyl%20PVC">4" x 4"</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=3%2Bin.%2Bx%2B3%2Bin.&amp;coating=Vinyl%20PVC">3" x 3"</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=2%2Bin.%2Bx%2B4%2Bin.&amp;coating=Vinyl%20PVC">2" x 4"</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=2%2Bin.%2Bx%2B2%2Bin.&amp;coating=Vinyl%20PVC">2" x 2"</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=2%2Bin.%2Bx%2B2%2Bin.&amp;coating=Vinyl%20PVC">1 1/2" x 1 1/2"</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=1%2B1%252F2%2Bin.%2Bx%2B4%2Bin.&amp;coating=Vinyl%20PVC">1 1/2" x 4"</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=3%2Bin.%2Bx%2B2%2Bin.&amp;coating=Vinyl%20PVC">3" x 2"</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=1%2Bin.%2Bx%2B3%2Bin.&amp;coating=Vinyl%20PVC">1" x 3"</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=1%2Bin.%2Bx%2B2%2Bin.&amp;coating=Vinyl%20PVC">1" x 2"</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=1%2Bin.%2Bx%2B1%2Bin.&amp;coating=Vinyl%20PVC">1" x 1"</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=1%252F2%2Bin.%2Bx%2B1%2Bin.&amp;coating=Vinyl%20PVC">1/2" x 1"</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=1%252F2%2Bin.%2Bx%2B1%252F2%2Bin.&amp;coating=Vinyl%20PVC">1/2" Hardware Cloth</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=1%252F4%2Bin.%2Bx%2B1%252F4%2Bin.&amp;coating=Vinyl%20PVC">1/4" Hardware Cloth</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=1%252F8%2Bin.%2Bx%2B1%252F8%2Bin.&amp;coating=Vinyl%20PVC">1/8" Hardware Cloth</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=1%2Bin.%2Bx%2B1%2Bin.&coating=Vinyl%20PVC">1" Hex Netting/Chicken Wire</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=2%2Bin.%2Bx%2B2%2Bin.&coating=Vinyl%20PVC">2" Hex Netting/Chicken Wire</a></li>
+                            <li><a class="dropdown-item" href="http://192.168.0.135/wwf-product?meshSize=4%2Bin.%2Bx%2B4%2Bin.&amp;coating=Galvanized">Knock-In Post</a></li>
                             <li><a class="dropdown-item" href="#">Fence Pen Kits</a></li>
-                            <!-- <li><a class="dropdown-item" href="#">Welded Wire Samples</a></li> -->
                         </ul>
                     </div>
 
@@ -233,15 +190,12 @@
                         <a href='{{ route('category.show', ['slug' => $page->slug]) }}'
                             class="nav-link btn nav-btn">{{ strtoupper($page->title ?: $page->category->family_category_name) }}</a>
                     @endforeach
-                    <a href="{{ route('contact') }}" class="nav-link btn nav-btn">
-                        CONTACT US
-                    </a>
-
+                    
+                    <a href="{{ route('contact') }}" class="nav-link btn nav-btn">CONTACT US</a>
 
                     <div class="dropdown">
                         <a href="#" class="nav-link btn nav-btn dropdown-toggle" data-bs-toggle="dropdown">
-                            <i class="bi bi-list fs-2 fs-md-4"></i>
-                            <span class="d-none d-md-inline"> Quick Menu</span>
+                            <i class="bi bi-list"></i> Quick Menu
                         </a>
                         <ul class="dropdown-menu">
                             @foreach(\App\Models\CategoryPage::with('category')->where('menu_type', 'quick_menu')->orderBy('menu_order')->get() as $page)
@@ -255,8 +209,7 @@
                                 <a class="dropdown-item" href="{{ route('post-caps') }}">Post Caps</a>
                             </li>
                             <li>
-                                <a class="dropdown-item" href="{{ route('temp-construction-fence') }}">Temp Construction
-                                    Fence</a>
+                                <a class="dropdown-item" href="{{ route('temp-construction-fence') }}">Temp Construction Fence</a>
                             </li>
                         </ul>
                     </div>
@@ -270,14 +223,15 @@
                             <a href="{{ route('woodfence') }}" class="nav-link btn nav-btn dropdown-toggle"
                                 data-bs-toggle="dropdown">WOOD FENCE</a>
                             <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="{{ route('stockade.index') }}">Stockade Fence</a></li>
-                            <li><a class="dropdown-item" href="{{ route('board-on-board') }}">Board Fence</a></li>
-                            <li><a class="dropdown-item" href="{{ route('tongue-groove') }}">Tongue Groove Fence</a></li>
-                            <li><a class="dropdown-item" href="{{ route('postrail.index') }}">Posts & Rails</a></li>
+                                <li><a class="dropdown-item" href="{{ route('stockade.index') }}">Stockade Fence</a></li>
+                                <li><a class="dropdown-item" href="{{ route('board-on-board') }}">Board Fence</a></li>
+                                <li><a class="dropdown-item" href="{{ route('tongue-groove') }}">Tongue Groove Fence</a></li>
+                                <li><a class="dropdown-item" href="{{ route('postrail.index') }}">Posts & Rails</a></li>
                             </ul>
                         </div>
 
                         <a href="#" class="nav-link btn nav-btn mb-2">VINYL FENCE</a>
+                        
                         <!-- Chain Link Fence Dropdown -->
                         <div class="dropdown mb-2">
                             <a href="{{ route('chainlink.main') }}" class="nav-link btn nav-btn dropdown-toggle" data-bs-toggle="dropdown">CHAIN LINK</a>
@@ -299,11 +253,14 @@
                         </div>
 
                         <a href='{{ route('weldedwire') }}' class="nav-link btn nav-btn mb-2">WELDED WIRE</a>
+                        
                         @foreach(\App\Models\CategoryPage::with('category')->where('menu_type', 'main_menu')->orderBy('menu_order')->get() as $page)
                             <a href='{{ route('category.show', ['slug' => $page->slug]) }}'
                                 class="nav-link btn nav-btn mb-2">{{ strtoupper($page->title ?: $page->category->family_category_name) }}</a>
                         @endforeach
+                        
                         <a href='{{ route('contact') }}' class="nav-link btn nav-btn mb-2">CONTACT US</a>
+                        
                         <div class="dropdown">
                             <a href="#" class="nav-link btn nav-btn dropdown-toggle" data-bs-toggle="dropdown">
                                 <i class="bi bi-list"></i> Quick Menu
@@ -322,6 +279,76 @@
                     </div>
                 </div>
             </div>
+
+            <style>
+                .desktop-nav {
+                    background-color: #f8f9fa;
+                }
+                
+                .nav-menu {
+                    display: flex;
+                    flex-wrap: nowrap;
+                    list-style: none;
+                    padding: 0;
+                    margin: 0;
+                    overflow-x: auto;
+                    white-space: nowrap;
+                }
+                
+                .nav-menu::-webkit-scrollbar {
+                    height: 5px;
+                }
+                
+                .nav-menu::-webkit-scrollbar-thumb {
+                    background-color: rgba(0,0,0,0.2);
+                    border-radius: 10px;
+                }
+                
+                .nav-item {
+                    display: inline-block;
+                    margin-right: 5px;
+                }
+                
+                .nav-link {
+                    display: inline-block;
+                    padding: 8px 15px;
+                    color: #212529;
+                    text-decoration: none;
+                    background-color: #e9ecef;
+                    border-radius: 4px;
+                    font-weight: 500;
+                    transition: all 0.3s ease;
+                }
+                
+                .nav-link:hover {
+                    background-color: #dee2e6;
+                    color: #000;
+                }
+                
+                .dropdown-toggle::after {
+                    display: inline-block;
+                    margin-left: 5px;
+                    vertical-align: middle;
+                    content: "";
+                    border-top: 5px solid;
+                    border-right: 5px solid transparent;
+                    border-bottom: 0;
+                    border-left: 5px solid transparent;
+                }
+                
+                .dropdown-menu {
+                    border-radius: 4px;
+                    box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
+                }
+                
+                .dropdown-item {
+                    padding: 8px 15px;
+                }
+                
+                .dropdown-item:hover {
+                    background-color: #e9ecef;
+                }
+            </style>
 
             <!-- Breadcrumb and NJ Fence Installation Guide -->
             <div class="d-flex flex-column flex-md-row justify-content-between align-items-center mt-2">
